736f057d92579851b4f8043eaef653483cb6d6f3
Chris Juelfs <CRJuelfs24@comcast.net>
2019-05-21 19:34:36 -0500

    deleted build from .gitignore

---
+++ HouseCup/house-cup-app/build/precache-manifest.c751e38584e9fc89403c3490c2139ecf.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "018838cb2f4612f99779db287d765bbd",
+    "url": "/static/media/BigJohnPRO-Bold.018838cb.otf"
+  },
+  {
+    "revision": "eba1f62c83cad63a7660",
+    "url": "/static/css/main.a327e7ac.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "3b783989ee1047beed9e",
+    "url": "/static/js/2.3b783989.chunk.js"
+  },
+  {
+    "revision": "de6187afdde39988e7a64c392c0dd191",
+    "url": "/static/media/realestate.de6187af.png"
+  },
+  {
+    "revision": "9ddd8aa1ef4dcf1ca2490bf4504a2fd9",
+    "url": "/static/media/dumbledore.9ddd8aa1.jpg"
+  },
+  {
+    "revision": "eba1f62c83cad63a7660",
+    "url": "/static/js/main.eba1f62c.chunk.js"
+  },
+  {
+    "revision": "9b550f15f8938c843c72650d7bad6034",
+    "url": "/static/media/BigJohnPRO-Regular.9b550f15.otf"
+  },
+  {
+    "revision": "de6928a8170976eefbd1eb77ba7db3a5",
+    "url": "/static/media/icon-phone.de6928a8.svg"
+  },
+  {
+    "revision": "b01dff5074cde00cf47819bf789481be",
+    "url": "/static/media/icon-chart.b01dff50.svg"
+  },
+  {
+    "revision": "009ef44ad05d1f3b494571dd9f93e7eb",
+    "url": "/static/media/icon-teacher.009ef44a.svg"
+  },
+  {
+    "revision": "93c0f233a4e4655bf01890069c689bcb",
+    "url": "/static/media/castle.93c0f233.svg"
+  },
+  {
+    "revision": "4de6d67100770146ca459f5814052131",
+    "url": "/index.html"
+  }
+];
